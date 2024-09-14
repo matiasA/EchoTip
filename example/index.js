5994234684390da579c99bec8183e335d3af89ea
Matias A <matiasprogramador@gmail.com>
2024-09-14 11:41:55 -0300

    chore: configura entorno de desarrollo y ejemplo

---
+++ example/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExamplePage from './ExamplePage';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ExamplePage />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
